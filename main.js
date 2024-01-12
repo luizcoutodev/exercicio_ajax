229fab274a115bc033525a6f7b7645ae9439f84f
Luiz <luiz.fcds1@gmail.com>
2024-01-12 09:51:46 -0300

    fixed try catch

--- main.js
+++ main.js
@@ -7,20 +7,24 @@ document.addEventListener('DOMContentLoaded', function(){
     const followers = document.querySelector('#followers');
     const following = document.querySelector('#following');
 
-    // fetch com a api do githube logo após retornando a resposta -> then (então) continua a função.
-    fetch('https://api.github.com/users/luizcoutodev')
-    .then(function(res){
-        return res.json();
-        // puxou o arquivo .json da api com os dados. 
-    }) 
-    .then(function(json){
-        // está lendo o arquivo json que foi gerado [O QUE RETORNOU]
-        avatar.src = json.avatar_url;
-        name.innerText = json.name;
-        username.innerText = `@${json.login}`;
-        bio.innerText = json.bio;
-        repos.innerText = json.public_repos;
-        followers.innerText = json.followers;
-        following.innerText = json.following;
-    })
+    try {
+        // fetch com a api do github logo após retornando a resposta -> then (então) continua a função.
+        fetch('https://api.github.com/users/luizcoutodev')
+        .then(function(res){
+            return res.json();
+            // puxou o arquivo .json da api com os dados. 
+        }) 
+        .then(function(json){
+            // está lendo o arquivo json que foi gerado [O QUE RETORNOU]
+            avatar.src = json.avatar_url;
+            name.innerText = json.name;
+            username.innerText = `@${json.login}`;
+            bio.innerText = json.bio;
+            repos.innerText = json.public_repos;
+            followers.innerText = json.followers;
+            following.innerText = json.following;
+        })
+    } catch (error) {
+        alert('erro!');
+    }
 })
